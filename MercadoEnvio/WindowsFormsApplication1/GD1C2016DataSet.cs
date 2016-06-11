0f1c35f61559079954a2137ed84f3e1a0420c871
Agustín Cipriano <agustincipriano@hotmail.com>
2016-06-11 19:05:29 -0300

    Arreglo ABM de rol que quedaba mal

--- MercadoEnvio/WindowsFormsApplication1/GD1C2016DataSet.cs
+++ MercadoEnvio/WindowsFormsApplication1/GD1C2016DataSet.cs
@@ -13,3 +13,15 @@ namespace WindowsFormsApplication1
 namespace WindowsFormsApplication1
 {
 }
+namespace WindowsFormsApplication1 {
+    
+    
+    public partial class GD1C2016DataSet {
+    }
+}
+namespace WindowsFormsApplication1 {
+    
+    
+    public partial class GD1C2016DataSet {
+    }
+}
