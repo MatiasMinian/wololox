9dad5686216d39fa0ec8237de259f6c5d37ea3eb
Agustín Cipriano <agustincipriano@hotmail.com>
2016-06-17 18:18:43 -0300

    Arreglo del login y usuarios

--- MercadoEnvio/WindowsFormsApplication1/Login.cs
+++ MercadoEnvio/WindowsFormsApplication1/Login.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApplication1
 {
@@ -53,27 +54,39 @@ namespace WindowsFormsApplication1
             {
 
                 //Hashear contraseña
-                SHA256 CriptoPass = SHA256Managed.Create();
-                byte[] valorHash;
-                valorHash = CriptoPass.ComputeHash(obtenerNumBytes(textoPass.Text));
+                try
+                {
+                    SHA256 CriptoPass = SHA256Managed.Create();
+                    byte[] valorHash;
+                    valorHash = CriptoPass.ComputeHash(obtenerNumBytes(textoPass.Text));
+                    if (Convert.ToBoolean(adapterUsuarios.login(textoUser.Text, valorHash)))
+                    {
+                        Menu_Cliente menu = new Menu_Cliente();
+                        //Menu_Administradores menu = new Menu_Administradores();
+                        menu.matchearUsuario(textoUser.Text);
+                        this.Hide();
+                        menu.ShowDialog();
+                        this.Close();
+                        this.Dispose();
+                    }
 
-               switch ((int)(adapterUsuarios.login(textoUser.Text, textoPass.Text))){
-                    case 0: MessageBox.Show("Usuario/Contraseña incorrectos!");
-                        break;
-                    case 1: loguearse(textoUser.Text);
-                        break;
-                    case 2: MessageBox.Show("Usuario bloqueado!");
-                        break;
                 }
-               
 
-               Menu_Cliente menu = new Menu_Cliente();
-               //Menu_Administradores menu = new Menu_Administradores();
-                menu.matchearUsuario(textoUser.Text);
-                this.Hide();
-                menu.ShowDialog();
-                this.Close();
-                this.Dispose();
+                catch (SqlException ex)
+                {
+                    switch (ex.Number)
+                    {
+                        case 40003:
+                            MessageBox.Show("Password incorrecta", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                            return;
+                        case 40002:
+                            MessageBox.Show("Usuario Bloqueado", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                            return;
+                        case 40001:
+                            MessageBox.Show("El Usuario no existe", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                            return;
+                    }
+                }
 
             }
 
@@ -88,7 +101,8 @@ namespace WindowsFormsApplication1
             }
             else
             {
-                switch ((String)adapterUsuarios.obtenerRol(User)){
+                switch ((String)adapterUsuarios.obtenerRol(User))
+                {
                     case "Empresa": Menu_Empresa menu = new Menu_Empresa();
                         break;
                     case "Cliente": Menu_Cliente menu2 = new Menu_Cliente();
@@ -96,7 +110,7 @@ namespace WindowsFormsApplication1
                     //Ver que hacer con nuevos roles, ver cambiar este malicioso switch
                 }
             }
-            
+
         }
 
         static byte[] obtenerNumBytes(string input)
