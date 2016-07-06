05de5eb536992e7d619528e3a34b35fe7ac6b209
Agustín Cipriano <agustincipriano@hotmail.com>
2016-07-06 16:42:33 -0300

    Merge

--- MercadoEnvio/WindowsFormsApplication1/Program.cs
+++ MercadoEnvio/WindowsFormsApplication1/Program.cs
@@ -30,7 +30,7 @@ namespace WindowsFormsApplication1
             
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Pantalla_Rol_Principal());
+            Application.Run(new Pantalla_Rol_Principal());
             //Application.Run(new FormularioLogin());
 
         }
